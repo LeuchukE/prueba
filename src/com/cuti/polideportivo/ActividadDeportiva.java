8202d36bf97aedc06ffddfaccb0e33ce662cbf7f
LeuchukE <eugenialeuchuk@gmail.com>
2014-01-20 13:24:57 -0800

    Update ActividadDeportiva.java

--- src/com/cuti/polideportivo/ActividadDeportiva.java
+++ src/com/cuti/polideportivo/ActividadDeportiva.java
@@ -5,7 +5,7 @@ public class ActividadDeportiva {
 	private int codigo;
 	private String nombre;
 	
-	//comments one
+	//comment one
 	
 	public ActividadDeportiva(int codigo, String nombre) {
 		this.codigo = codigo;
